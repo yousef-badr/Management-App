b5fbe57648c668ed924b17b5143d950e7891e3c4
Yousef badr <yousefbadr10@gmail.com>
2024-09-09 21:38:33 +0300

    controllers

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,31 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TaskController;
+use App\Http\Controllers\UserController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+//Route::get('/', function () {
+//    return Inertia::render('Welcome', [
+//        'canLogin' => Route::has('login'),
+//        'canRegister' => Route::has('register'),
+//        'laravelVersion' => Application::VERSION,
+//        'phpVersion' => PHP_VERSION,
+//    ]);
+//});
+
+Route::redirect('/','/dashboard');
+
+Route::middleware(['verified','auth'])->group(function (){
+   Route::get('/dashboard',fn() => Inertia::render('Dashboard'))
+       ->name('dashboard');
+
+   Route::resource('project',ProjectController::class);
+   Route::resource('user',UserController::class);
+   Route::resource('task',TaskController::class);
 });
 
 Route::get('/dashboard', function () {
